5e40345c2b7e11bd1c12e72e49b12dac0bea5554
sjkim2322 <sjkim2322@naver.com>
2017-08-10 10:45:44 +0900

    Create datasample.sql

---
+++ datasample.sql
@@ -0,0 +1,168 @@
+insert into categories(id, name) value (1,'전시');
+insert into categories(id, name) value (2,'뮤지컬');
+insert into categories(id, name) value (3,'콘서트');
+insert into categories(id, name) value (4,'클래식');
+insert into categories(id, name) value (5,'연극');
+
+
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (1, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (1, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (2, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (2, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (4, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");   
+            insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (1, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (1, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (2, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (2, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (4, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");
+insert into products
+        (category_id, represent_file_id, name, description, sales_start, sales_end, sales_flag, event)
+    values
+            (3, 1, "서울광화문", "정말 재밌는 전시입니다.", "2017-01-01 23:50:40", "2017-01-10 23:50:40", 0, "event 정보");            
+
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(1, 1, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 1', '장소지번 1', '장소도로주소 1', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(2, 2, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 2', '장소지번 2', '장소도로주소 2', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(3, 3, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 3', '장소지번 3', '장소도로주소 3', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(4, 4, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 4', '장소지번 4', '장소도로주소 4', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(5, 5, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 5', '장소지번 5', '장소도로주소 5', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(6, 6, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 6', '장소지번 6', '장소도로주소 6', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(7, 7, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 7', '장소지번 7', '장소도로주소 7', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(8, 8, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 8', '장소지번 8', '장소도로주소 8', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(9, 9, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 9', '장소지번 9', '장소도로주소 9', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(10, 10, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 10', '장소지번 10', '장소도로주소 10', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(11, 11, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 11', '장소지번 11', '장소도로주소 11', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+insert into products_display(id, product_id, observation_time, display_start, display_end, place_name, place_lot, place_street, tel, homepage, email, create_date, modify_date)
+values(12, 12, '09:00-18:00', '2017-01-01 23:50:40', '2017-01-10 23:50:40', '장소명 12', '장소지번 12', '장소도로주소 12', '010-1111-1111', 'http://www.naver.com', 'carami@nate.com', now(), now());
+
+
+
+
+INSERT INTO products_detail
+        (product_id, content)
+    VALUES
+            (3, "내용이 없습니다.");
+    INSERT INTO products_detail
+        (product_id, content)
+    VALUES
+            (4, "내용이 없습니다.");
+    INSERT INTO products_detail
+        (product_id, content)
+    VALUES
+            (5, "내용이 없습니다.");
+    INSERT INTO products_detail
+        (product_id, content)
+    VALUES
+            (6, "내용이 없습니다.");
+    INSERT INTO products_detail
+        (product_id, content)
+    VALUES
+            (7, "내용이 없습니다.");
+    INSERT INTO products_detail
+        (product_id, content)
+    VALUES
+            (8, "내용이 없습니다.");
+    INSERT INTO products_detail
+        (product_id, content)
+    VALUES
+            (9, "내용이 없습니다.");
+    INSERT INTO products_detail
+        (product_id, content)
+    VALUES
+            (10, "내용이 없습니다.");
