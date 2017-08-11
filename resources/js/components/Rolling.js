1eb23cbde149452cff0af0ace84cad3f285e7976
KIMSEONGJIN <sjkim2322@naver.com>
2017-08-11 23:19:31 +0900

    롤링 컴포넌트, 메인페이지 타이머 모듈 추가 롤링 컴포넌트 와 메인페이지 타이머 분리

--- resources/js/components/Rolling.js
+++ resources/js/components/Rolling.js
@@ -1,122 +1,84 @@
 define(['Extend', 'Component'], function (extend, Component) {
 
     var Rolling = extend(Component, {
-        "init": function (rootTarget, timeoutFlag, prev, next) {
-            this.rollingCount = 0;
-            this.intervalId;
-            this.timeoutId;
-            this.rollingSize = $(rootTarget).find(".visual_img > li").length - 1;
-            this.rollingFlag = 0;
-            this.width = $(rootTarget).find(".visual_img > li").width();
-            this.rootTarget = rootTarget;
-            this.cycleFlag = 0;
-            this.timeoutFlag = timeoutFlag;
-            this.prev = prev;
-            this.next = next;
+        "init": function (rootTarget, prev, next) {
+            this.rootTarget = $(rootTarget);
+            this.prev = $(prev);
+            this.next = $(next);
+            this.rollingTargets = this.rootTarget.children();
+            this.targetCount = this.rollingTargets.length;
+            this.width = this.rollingTargets.width();
+            this.currentChildIndex;
+            this.isAnimating = false;
+            this.createFakeTarget();
             this.setEvent();
         }
     });
+    Rolling.prototype.createFakeTarget = function() {
+        var fakeFirstChild = this.rootTarget.children(':first-child').clone();
+        var fakeLastChild = this.rootTarget.children(':last-child').clone();
+        this.rootTarget.append(fakeFirstChild);
+        this.rootTarget.prepend(fakeLastChild);
+        this.currentChildIndex = 1;
+        this.rootTarget.css({"transform": "translate(-" + this.width*this.currentChildIndex + "px,0)"});
+
+    };
 
     Rolling.prototype.setEvent = function () {
         $(this.prev).on("click", this.clickPrev.bind(this));
         $(this.next).on("click", this.clickNext.bind(this));
-
     };
     Rolling.prototype.clickPrev = function (){
-        if (!this.rollingFlag) {
-            this.preRolling();
-            if (this.timeoutFlag) {
-                this.timeoutRolling();
-            } else {
-                ProductDetail.updateRollingCount(0); // 임시로 해놓은 것
-            }
+        if (this.isAnimating) {
+            return ;
+        } else {
+            this.isAnimating = true;
+            this.animator('prev');
         }
+
     };
     Rolling.prototype.clickNext = function (){
-            if (!this.rollingFlag) {
-                this.nxtRolling();
-                if (this.timeoutFlag) {
-                    this.timeoutRolling();
-                } else {
-                    ProductDetail.updateRollingCount(1); // 임시로 해놓은 것
-                }
+            if (this.isAnimating) {
+                return ;
+            } else {
+                this.isAnimating = true;
+                this.animator('next');
             }
     };
-    Rolling.prototype.nxtRolling = function () {
-        var $img = this.rootTarget.find(".visual_img");
 
-        if (this.rollingCount < this.rollingSize) {
-            $img.animate({"left": "-=" + this.width}, {
-                duration: 500,
-                start: function () {
-                    this.rollingFlag = 1;
-                }.bind(this),
-                complete: function () {
-                    this.rollingFlag = 0;
-                    this.rollingCount++;
-                }.bind(this)
-            });
-        } else if (this.cycleFlag) {
-            $img.css("left", "0px");
-            $img.animate({"left": "-=" + this.width}, {
-                duration: 500,
-                start: function () {
-                    this.rollingFlag = 1;
-                }.bind(this),
-                complete: function () {
-                    this.rollingFlag = 0;
-                    this.rollingCount = 0;
-                }.bind(this)
-            });
+    //direction value is "next" or "prev"
+    Rolling.prototype.animator = function(direction) {
+        var moveSize;
+        var moveIndex;
+        if(direction === "next") {
+            moveSize = -this.width;
+            moveIndex = 1;
+        } else if(direction === "prev") {
+            moveSize = this.width;
+            moveIndex = -1;
         }
-    };
-
-    Rolling.prototype.preRolling = function () {
-        var $img = this.rootTarget.find(".visual_img");
-
-        if (this.rollingCount > 0) {
-            $img.animate({"left": "+=" + this.width}, {
-                duration: 500,
-                start: function () {
-                    this.rollingFlag = 1;
+        this.rootTarget.animate(
+            {move : moveSize},
+            {
+                duration : 1000,
+                step : function(now) {
+                    this.rootTarget.css({"transform": "translate(" + (-(this.width*this.currentChildIndex)+now) + "px,0)"});
                 }.bind(this),
                 complete: function () {
-                    this.rollingFlag = 0;
-                    this.rollingCount--;
+                    this.currentChildIndex += moveIndex;
+                    if(this.currentChildIndex === 0 || this.currentChildIndex === this.targetCount+1) {
+                        if (this.currentChildIndex === 0) {
+                            this.currentChildIndex = this.targetCount;
+                        } else if (this.currentChildIndex === this.targetCount + 1) {
+                            this.currentChildIndex = 1;
+                        }
+                        this.rootTarget.css({"transform": "translate(-" + this.width * this.currentChildIndex + "px,0)"})
+                    }
+                    delete this.rootTarget.get(0).move;
+                    this.isAnimating = false;
                 }.bind(this)
             });
-        } else if (this.cycleFlag) {
-            $img.animate({"left": "+=" + this.width}, {
-                duration: 500,
-                start: function () {
-                    this.rollingFlag = 1;
-                }.bind(this),
-                complete: function () {
-                    $img.css('left', -(this.width * (this.rollingSize + 1)));
-                    this.rollingFlag = 0;
-                    this.rollingCount = this.rollingSize;
-                }.bind(this)
-            });
-        }
     };
-
-    Rolling.prototype.offRolling = function (){
-        this.rootTarget.off("");
-    };
-
-    Rolling.prototype.timeoutRolling = function () {
-        clearInterval(this.intervalId);
-        clearTimeout(this.timeoutId);
-
-        this.timeoutId = setTimeout(function () {
-            this.intervalId = setInterval(this.nxtRolling.bind(this), 2000);
-        }.bind(this), 4000);
-    };
-    Rolling.prototype.intervalRolling = function () {
-        this.intervalId = setInterval(this.nxtRolling.bind(this), 2000);
-    };
-    // rolling에 flag를 줘서 이벤트 발생 도중이면 이벤트를 스킵
-    // animate에 callback함수 등록
 
     return Rolling;
 });
