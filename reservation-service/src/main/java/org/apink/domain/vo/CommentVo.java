dfc9287e1e96b7f4f403da6d23c2774c9f917db7
KIMSEONGJIN <sjkim2322@naver.com>
2017-08-16 22:29:40 +0900

    conflict clear

--- reservation-service/src/main/java/org/apink/domain/vo/CommentVo.java
+++ reservation-service/src/main/java/org/apink/domain/vo/CommentVo.java
@@ -17,21 +17,12 @@ public class CommentVo extends Comment {
     public void setImages(List<Integer> images) {
         this.images = images;
     }
-<<<<<<< HEAD
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-=======
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
->>>>>>> juhyung
     }
 }
--- reservation-service/src/main/java/org/apink/mapper/dao/sql/CommentSql.java
+++ reservation-service/src/main/java/org/apink/mapper/dao/sql/CommentSql.java
@@ -3,14 +3,7 @@ package org.apink.mapper.dao.sql;
 public class CommentSql {
 
     public final static String SELECT_BY_PRODUCT_ID =
-<<<<<<< HEAD
-            "SELECT id, " +
-                    "score, " +
-                    "comment, " +
-                    "product_id " +
-                    "FROM comments " +
-                    "WHERE product_id=:product_id " +
-=======
+
             "SELECT c.id, "+
                     "c.score, "+
                     "c.comment, "+
@@ -20,7 +13,6 @@ public class CommentSql {
                     "FROM comments AS c JOIN Users AS u " +
                     "ON c.user_id = u.id "+
                     "WHERE c.product_id= :product_id "+
->>>>>>> juhyung
                     "LIMIT :pagePerNum OFFSET :offset;";
 
 }
